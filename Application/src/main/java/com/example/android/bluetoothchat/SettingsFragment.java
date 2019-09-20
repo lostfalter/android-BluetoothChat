05cbd1bc4eb3fe549bc1cb9719e01f048899545c
lostfly <lostfalter@gmail.com>
2019-09-21 02:06:44 +0800

    Set dynamic summary for edit text preference

--- Application/src/main/java/com/example/android/bluetoothchat/SettingsFragment.java
+++ Application/src/main/java/com/example/android/bluetoothchat/SettingsFragment.java
@@ -1,12 +1,48 @@
 package com.example.android.bluetoothchat;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 public class SettingsFragment extends PreferenceFragment {
+    private SharedPreferences.OnSharedPreferenceChangeListener onSharedPreferenceChangeListener =
+        new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                Preference pref = findPreference(key);
+                if (pref instanceof EditTextPreference) {
+                    EditTextPreference etp = (EditTextPreference) pref;
+                    pref.setSummary(etp.getText() + " 秒");
+                }
+            }
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        SharedPreferences sp = getPreferenceScreen().getSharedPreferences();
+        setSummaryOfPreference(sp, "red_light_time");
+        setSummaryOfPreference(sp, "yellow_light_time");
+        setSummaryOfPreference(sp, "green_light_time");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
+    }
+
+    private void setSummaryOfPreference(SharedPreferences sharedPreferences, String key) {
+        findPreference(key).setSummary(sharedPreferences.getString(key, "") + " 秒");
     }
 }
